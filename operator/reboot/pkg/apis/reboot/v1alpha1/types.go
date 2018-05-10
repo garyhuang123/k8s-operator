e3344b0ce92b31e6a179f528b9e37014cab301c9
gary huang <Gary.Huang@ehealth.com>
2018-05-10 11:12:41 +0800

    change reboot types.go

--- operator/reboot/pkg/apis/reboot/v1alpha1/types.go
+++ operator/reboot/pkg/apis/reboot/v1alpha1/types.go
@@ -6,22 +6,30 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 type Reboot struct {
-	metav1.TypeMeta `json:",inline"`
-	// +optional
+	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// +optional
-	Status RebootStatus `json:"status,omitempty"`
+
+	Spec   RebootSpec   `json:"spec"`
+	Status RebootStatus `json:"status"`
 }
 
+// RebootSpec is the spec for a Reboot resource
+type RebootSpec struct {
+	DeploymentName string `json:"deploymentName"`
+	Replicas       *int32 `json:"replicas"`
+}
+
+// RebootStatus is the status for a Reboot resource
+type RebootStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RebootList is a list of Reboot resources
 type RebootList struct {
 	metav1.TypeMeta `json:",inline"`
-	// +optional
-	metav1.ListMeta `json:"metadata,omitempty"`
+	metav1.ListMeta `json:"metadata"`
 
 	Items []Reboot `json:"items"`
-}
-
-type RebootStatus struct {
-	Available   int
-	Unavailable int
 }
